bd157deb0bb4898b5491078cc9dff452130c31e8
Menchu MV <mcmvillalobos@gmail.com>
2021-11-12 13:57:23 +0100

    Added cleanFolder functionality and called it in IIFE

--- index.js
+++ index.js
@@ -6,8 +6,44 @@ console.log(chalk.yellow("hola peix"));
 
 const cleanFolder = (directory) => {
   fs.readdir(directory, (error, files) => {
-    console.log(files);
+    if (error) {
+      if (error.code === "ENOENT") {
+        error.message = "ERROR. Introduce una carpeta que exista.";
+      } else if (error.code === "ENOTDIR") {
+        error.message = "ERROR. No es una carpeta.";
+      }
+      console.log(chalk.red(error.message));
+      process.exit(1);
+    }
+    if (files.length === 0) {
+      console.log(chalk.red("Aquí no hay nada"));
+    } else {
+      for (const file of files) {
+        fs.statSync(directory + "/" + file).isDirectory()
+          ? cleanFolder(path.join(directory, file))
+          : fs.unlink(path.join(directory, file), (error) => {
+              if (error) {
+                console.log(chalk.red(error.message));
+                process.exit(1);
+              }
+            });
+        console.log(chalk.redBright("File deleted: " + file));
+      }
+    }
+  });
+
+  fs.rmdir(directory, { recursive: true }, (error) => {
+    if (error) {
+      console.log(chalk.red(error.message));
+      process.exit(1);
+    }
+    console.log(chalk.redBright("File deleted: " + directory));
   });
 };
 
-cleanFolder(".");
+(async () => {
+  await cleanFolder("./Finals");
+  console.log(chalk.green("Enhorabuena, has borrado todo"));
+})();
+
+//cleanFolder("./Finals");
