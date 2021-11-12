fb139e594b80f541984e41a3aa27cc8dd649c845
Menchu MV <mcmvillalobos@gmail.com>
2021-11-12 12:20:42 +0100

    Added function that reads all files in a folder

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const path = require("path");
 const chalk = require("chalk");
 
 console.log(chalk.yellow("hola peix"));
+
+const cleanFolder = (directory) => {
+  fs.readdir(directory, (error, files) => {
+    console.log(files);
+  });
+};
+
+cleanFolder(".");
